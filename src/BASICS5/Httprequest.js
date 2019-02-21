d092acbd9e52ddc7e555bf4f3fbe5e8f1cb11848
metho <metholk@gmail.com>
2019-02-21 16:26:05 +0530

    Added a new state for the details and made a get request

--- src/BASICS5/Httprequest.js
+++ src/BASICS5/Httprequest.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
  class Httprequest extends Component {
+     constructor(props) {
+       super(props)
+     
+       this.state = {
+          title : "Email List",
+          details : []
+       }
+     }
+     componentDidMount(){
+         axios.get('https://jsonplaceholder.typicode.com/posts')
+     }
+     
   render() {
+      var {title} = this.state
     return (
       <div>
-        
+        <h1>{title}</h1>
       </div>
     )
   }
