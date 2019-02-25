b8728401538d22e1386513113917eaed706dfa0e
metho <metholk@gmail.com>
2019-02-25 09:10:15 +0530

    added input element

--- src/BASICS4/RefsDemo.js
+++ src/BASICS4/RefsDemo.js
@@ -4,10 +4,20 @@ import React, { Component } from 'react'
   render() {
     return (
       <div>
-        
+        <input
+        text="text"
+        />
       </div>
     )
   }
 }
 
 export default RefsDemo
+
+/* Concept of reps -
+Basically refs make it possible to access dom nodes directly within react
+there are two use cases of refs
+1. point a cursor in the input box
+2. to fetch the input value
+
+*/
